c9a5ad135f11c102f6b7ffd9ebe386c2d880d395
ShinkarenkoMaxim <shinkarenko.max97@gmail.com>
2022-06-11 23:02:07 +0500

    Fix username argument

--- src/utils/args.js
+++ src/utils/args.js
@@ -1,11 +1,20 @@
 export const getUsername = () => {
   let username = '';
   const args = process.argv.slice(2);
+  const hasUsernameArg = args && args[0].startsWith('--username');
 
   // Check if username is specified
-  if (args && args.includes('--username')) {
-    const argsId = args.indexOf('--username');
-    username = args[argsId + 1];
+  if (hasUsernameArg) {
+    const hasEqualSeparator = args[0].startsWith('--username=');
+    const hasSpaceSeparator = args[0] === '--username';
+
+    if (hasEqualSeparator) {
+      username = args[0].split('=')[1];
+    }
+
+    if (hasSpaceSeparator) {
+      username = args[1];
+    }
   } else {
     throw new Error('Username must be specified');
   }
